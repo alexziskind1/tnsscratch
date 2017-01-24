f297166f1bcef1595b20672e37fb6d591a61980f
Alexander Ziskind <alex@nuvious.com>
2017-01-24 14:13:08 -0500

    Floatbtn project simplification

--- floatbtn/app/app.component.ts
+++ floatbtn/app/app.component.ts
@@ -1,39 +1,15 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
-
-import { View } from 'ui/core/view';
-import { Button } from 'ui/button';
-import { TouchGestureEventData } from 'ui/gestures';
-import { Color } from 'color';
+import { Component } from "@angular/core";
 
 @Component({
     selector: "my-app",
-    templateUrl: "app2.component.html",
-    changeDetection: ChangeDetectionStrategy.OnPush
+    templateUrl: "app.component.html"
 })
 export class AppComponent {
     public arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
     private counter = 1;
-    public onTap(args: Button) {
-        this.counter ++;
+    public onTap(args) {
+        this.counter++;
         console.log('tapped ' + this.counter);
-    }
-
-    public onTouch(args: TouchGestureEventData) {
-        let theBtn = <Button>args.view;
-        switch (args.action) {
-            case 'down':
-                theBtn.animate({
-                    backgroundColor: new Color('red'),
-                    duration: 100
-                });
-                break;
-            case 'up':
-                theBtn.animate({
-                    backgroundColor: new Color('blue'),
-                    duration: 100
-                });
-                break;
-        }
     }
 }
--- floatbtn/app/float-btn.component.ts
+++ floatbtn/app/float-btn.component.ts
@@ -1,47 +1,20 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
-import { View } from 'ui/core/view';
 import { TouchGestureEventData, GestureEventData } from 'ui/gestures';
-import { Color } from 'color';
 
-declare var android: any;
 
 @Component({
     moduleId: module.id,
     selector: "float-btn",
     template: `
-    <StackLayout (tap)="onTap($event)" (touch)="onTouch($event)" (loaded)="onLoaded($event)" class="float-btn" >   
+    <StackLayout (tap)="onTap($event)" (touch)="onTouch($event)"  class="float-btn" >   
         <Label class="float-btn-text" [text]="text"></Label>
-    </StackLayout>
-
-
-    <StackLayout class="outer" (tap)="outerTap($event)" >
-        <StackLayout class="inner">
-            <Label class="inner-lbl" text="hi"></Label>
-        </StackLayout>
     </StackLayout>
     `,
     styles: [
         `
-            .outer {
-                background-color: green;
-                margin: 5;
-                padding: 5;
-            }
-            .inner {
-                background-color: red;
-                margin: 5;
-                padding: 5;
-            }
-            .float-btn-text {
-                margin: 5;
-                padding: 5;
-            }
-
-
             .float-btn {
                 background-color:#30bcff;
-                border-color:#30bcff;
                 border-radius: 28;
                 width:56;
                 height:56;
@@ -63,60 +36,15 @@ declare var android: any;
 
             @keyframes down {
                 from { background-color: #30bcff; }
-                to { background-color: red;  }
+                to { background-color: purple;  }
             }
         `
     ]
 })
-export class FloatBtnComponent implements OnInit {
+export class FloatBtnComponent {
 
     @Input() text: string;
     @Output() tap: EventEmitter<GestureEventData> = new EventEmitter<GestureEventData>();
-
-
-    public outerTap(args: GestureEventData) {
-        console.log('outerTap');
-    }
-
-    public ngOnInit() {
-        console.log('ngOnInit');
-    }
-
-    public onLoaded(args) {
-        //let obj = args.object;
-        //args.object.android.setBackgroundResource(android.R.drawable.dialog_holo_light_frame);
-        //args.object.android.setElevation(2);
-        //args.object.android.elevation = '2dp';
-        console.log('onLoaded');
-        console.log('backgorund before: ' + args.object.android.getBackground());
-
-        //args.object.android.setBackgroundColor(android.graphics.Color.parseColor("#000000"));
-
-//console.dir(android.R.drawable());
-
-        //args.object.android.setBackgroundResource(android.R.drawable.tags_rounded_corners);
-      
-        //let drawable = args.object.android.getBackground();
-
-        //drawable.setColor(android.graphics.Color.RED);
-console.log('constant color: ' + android.graphics.Color.RED);
-
-    var shape = new android.graphics.drawable.GradientDrawable();
-    shape.setShape(android.graphics.drawable.GradientDrawable.OVAL);
-    //shape.setCornerRadii([ 40, 40, 40, 40, 40, 40, 40, 40 ]);
-    shape.setColor(android.graphics.Color.RED);
-    //shape.setStroke(3, android.graphics.Color.RED);
-    args.object.android.setBackgroundDrawable(shape);
-
-
-        //args.object.android.setElevation(10);
-        args.object.android.setTranslationZ(10);
-        //args.object.android.elevation = 40;
-        //args.object.android.getChildAt(0).setElevation(20);
-        //args.object.android.getChildAt(0).elevation = 20;
-        //args.object.android.clipToPadding = false;
-        //console.log('elevation: ' + args.object.android.getChildAt(0).getElevation());
-    }
 
     public onTap(args: GestureEventData) {
         this.tap.emit(args);
@@ -129,7 +57,7 @@ console.log('constant color: ' + android.graphics.Color.RED);
                 theBtn.className = 'float-btn down';
                 break;
             case 'up':
-                //theBtn.className = 'float-btn';
+                theBtn.className = 'float-btn';
                 break;
         }
     }
