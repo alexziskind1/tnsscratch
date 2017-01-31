4be816fc96e5079ee483012500a0031a731b7542
Alexander Ziskind <alex@nuvious.com>
2017-01-30 19:08:59 -0500

    Floatbtn added native shadow to button

--- floatbtn/app/float-btn.component.ts
+++ floatbtn/app/float-btn.component.ts
@@ -1,25 +1,37 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 import { TouchGestureEventData, GestureEventData } from 'ui/gestures';
+import { Color } from 'color';
+
+
+declare const CGSizeMake: any;
+declare const android: any;
 
 
 @Component({
     moduleId: module.id,
     selector: "float-btn",
     template: `
-    <StackLayout (tap)="onTap($event)" (touch)="onTouch($event)"  class="float-btn" >   
-        <Label class="float-btn-text" [text]="text"></Label>
-    </StackLayout>
+    <GridLayout class="float-btn-wrapper">
+        <StackLayout (tap)="onTap($event)" (touch)="onTouch($event)" class="float-btn" (loaded)="onLoaded($event)">   
+            <Label class="float-btn-text" [text]="text"></Label>
+        </StackLayout>
+    </GridLayout>
     `,
     styles: [
         `
+            .float-btn-wrapper {
+                width:75;
+                height:75;
+            }
+
             .float-btn {
                 background-color:#30bcff;
                 border-radius: 28;
                 width:56;
                 height:56;
                 text-align: center;
-                vertical-align: middle;
+                vertical-align: middle;         
             }
 
              .float-btn.down {
@@ -46,6 +58,15 @@ export class FloatBtnComponent {
     @Input() text: string;
     @Output() tap: EventEmitter<GestureEventData> = new EventEmitter<GestureEventData>();
 
+
+    protected get shadowColor(): Color {
+        return new Color('#888888');
+    }
+
+    protected get shadowOffset(): number {
+        return 2.0;
+    }
+
     public onTap(args: GestureEventData) {
         this.tap.emit(args);
     }
@@ -59,6 +80,32 @@ export class FloatBtnComponent {
             case 'up':
                 theBtn.className = 'float-btn';
                 break;
+        }
+    }
+
+    public onLoaded(args) {
+        let tnsView = <any>args.object;
+
+        setTimeout(() => {
+            console.log('timeout done');
+            tnsView.clipToBounds = false;
+            if (tnsView.ios)
+                tnsView.ios.clipsToBounds = false;
+        }, 200);
+
+        if (tnsView.android) {
+            let nativeAnView = tnsView.android;
+            var shape = new android.graphics.drawable.GradientDrawable();
+            shape.setShape(android.graphics.drawable.GradientDrawable.OVAL);
+            shape.setColor(android.graphics.Color.parseColor("#30bcff"));
+            nativeAnView.setBackgroundDrawable(shape);
+            nativeAnView.setElevation(20);
+        } else if (tnsView.ios) {
+            let nativeView = tnsView.ios;
+            nativeView.layer.shadowColor = this.shadowColor.ios.CGColor;
+            nativeView.layer.shadowOffset = CGSizeMake(0, this.shadowOffset);
+            nativeView.layer.shadowOpacity = 0.5;
+            nativeView.layer.shadowRadius = 5.0;
         }
     }
 }
